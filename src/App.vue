6c7767c495e925c768f0448b4e6513f7722a6ec8
daniela <daniela.rodriguez@evertecinc.com>
2022-11-02 23:21:12 -0500

    change default routes

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,8 @@
 <template>
   <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+    <router-link to="/">Inicio</router-link> |
+    <router-link to="/search">Buscar Restaurante</router-link> |
+    <router-link to="/new">Crear Nuevo Restaurante</router-link>
   </nav>
   <router-view/>
 </template>
